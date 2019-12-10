af0b0d7cdc674c47e77bfdae73f0c0f51ced12f2
Your Name <melky@Melkys-iMac.local>
2019-12-10 08:56:38 -0500

    still need to finish fizbuz

---
+++ 4-functions/12-exercise.py
@@ -0,0 +1,9 @@
+def fizz_buzz(input):
+    print("")
+
+
+print(fizz_buzz(3))
+# if input is /3 return fizz
+# if input is /5 return buzz
+#  if input is /5/3 return fizz buzz
+# else return number
---
+++ 4-functions/7-**args.py
@@ -0,0 +1,5 @@
+def save_user(**user):
+    print(user["age"])
+
+# creates dictionary
+save_user(id=1, name="John", age=22)
